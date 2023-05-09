f85320a14c8f7ccb67ee93240fb0dfcf521657bc
remyajyjesh <remyajyjesh@gmail.com>
2023-05-08 20:20:04 -0600

    first commit

---
+++ src/main/java/com/example/awscicdpipeline/AwscicdpipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.awscicdpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwscicdpipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwscicdpipelineApplication.class, args);
+	}
+
+}
